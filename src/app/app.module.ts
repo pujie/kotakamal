d13718bcd6a3fe9d762ab2daffa764eae1eb9c74
pujie <pw.prayitno@gmail.com>
2019-11-17 21:28:33 +0700

    @ionic-native/geolocation is @ionic-native/geolocation/ngx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Geolocation } from '@ionic-native/geolocation'
+import { Geolocation } from '@ionic-native/geolocation/ngx'
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
