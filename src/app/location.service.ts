40ba5a14f46bcddf8bdb1738f3ad8990ab03d9af
pujie <pw.prayitno@gmail.com>
2019-11-25 21:45:13 +0700

    add phone

--- src/app/location.service.ts
+++ src/app/location.service.ts
@@ -32,7 +32,7 @@ export class LocationService {
       })
      };
     console.log("Data to save",loc)
-    let params = 'name='+loc.name+'&address='+loc.address+'&district='+loc.district+'&latitude='+loc.latitude+'&longitude='+loc.longitude+'&createuser='+loc.createuser+''
+    let params = 'name='+loc.name+'&address='+loc.address+'&district='+loc.district+'&phone='+loc.phone+'&latitude='+loc.latitude+'&longitude='+loc.longitude+'&createuser='+loc.createuser+''
     this.obj = this.http.post(this.appvar.getServer()+'save',params,ParseHeaders)
     this.obj.subscribe(
       data=>{
